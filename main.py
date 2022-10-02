33fce93712d5a2e90346bd9ad5a7048471b9f53c
Alex Nojenko <93285568+informationvulture@users.noreply.github.com>
2022-10-02 00:53:06 +0000

    Bug fixes (removed sound)

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import src.intro as i
-from replit import audio
+
 # If you need to call the API, run this:
 # fp.get_all_players()
 i.intro_txt()
 
 i.basic_return()
 
-source = audio.play_file("src/sounds/menu_music.mp3")
-
-while True:
-  pass
--- src/intro.py
+++ src/intro.py
@@ -2,8 +2,11 @@ from pprint import pprint
 import src.find_players as fp
 
 def intro_txt():
+  print("*********************************************")
+  print("\n")
   print("Welcome to Who Made That Dunk?")
   print("\n")
+  print("*********************************************")
 
 def basic_return():
   fname = input("Enter the first name you're searching for.")
@@ -12,3 +15,5 @@ def basic_return():
   values = fp.return_players(fname, lname)
   
   pprint(values, width=2)
+  if len(values) > 10:
+    print("Wow, so many players with the same name!")
